634ac7458b251682f6e9ddffd49256bdf6f1d73d
Sophia Byers <sophiabyers483@gmail.com>
2024-01-02 12:03:32 -0600

    Pulverizer refactoring.

--- Assets/Scripts/Machines/Pulverizer.cs
+++ Assets/Scripts/Machines/Pulverizer.cs
@@ -8,7 +8,7 @@ namespace Darkcast.Machines
     {
         [SerializeField] private Cookbook _cookbook;
 
-        private Recipe _currentRecipe;
+        private Recipe _activeRecipe;
         private Recipe _selectedRecipe;
 
         public int _energyNeededToCompleteCurrentRecipe;
@@ -18,7 +18,68 @@ namespace Darkcast.Machines
         public Inventory input = new();
 
         public Inventory output = new();
-        
+
+        private bool TrySetActiveRecipe()
+        {
+            // The machine already has an active recipe.
+            if (_activeRecipe)
+            {
+                return true;
+            }
+
+            // If there's no active recipe and no selected recipe then there's nothing to work on.
+            if (!_selectedRecipe)
+            {
+                return false;
+            }
+
+            // Check if the inventory contains all the ingredients for the selected recipe.
+            // ReSharper disable once ForeachCanBeConvertedToQueryUsingAnotherGetEnumerator
+            foreach (var ingredient in _selectedRecipe.ingredients)
+            {
+                if (!input.Contains(ingredient.item, ingredient.count))
+                {
+                    // The input inventory doesn't have everything needed work on the selected active recipe.
+                    return false;
+                }
+            }
+
+            // Remove all the ingredients from the inventory for the selected recipe.
+            foreach (var ingredient in _selectedRecipe.ingredients)
+            {
+                // Remove the items from the inventory.
+                input.Remove(ingredient.item, ingredient.count);
+            }
+
+            // Our input inventory has everything we need to keep working on this recipe.
+            _activeRecipe = _selectedRecipe;
+            _energyNeededToCompleteCurrentRecipe = _activeRecipe.energy;
+
+            return true;
+        }
+
+        private void WorkActiveRecipe()
+        {
+            // Work on the active recipe.
+            _energyNeededToCompleteCurrentRecipe -= 20;
+
+            // Check if the active recipe was completed.
+            if (_energyNeededToCompleteCurrentRecipe > 0)
+            {
+                // Active recipe was not completed.
+                return;
+            }
+
+            // Store all the resulting items in the output inventory.
+            foreach (var result in _activeRecipe.results)
+            {
+                output.Store(result.item, result.count);
+            }
+
+            // Reset the active recipe for the next tick.
+            _activeRecipe = null;
+        }
+
         public void SelectRecipe(Recipe recipe)
         {
             _selectedRecipe = recipe;
@@ -26,52 +87,9 @@ namespace Darkcast.Machines
 
         public void Tick()
         {
-            // If the machine does not have a recipe to work on then try and work on new recipe.
-            if (!_currentRecipe)
+            if (TrySetActiveRecipe())
             {
-                // If we don't have a selected recipe then we have nothing to work on.
-                if (!_selectedRecipe)
-                {
-                    return;
-                }
-
-                // Check if the inventory contains all the ingredients for the selected recipe.
-                // ReSharper disable once ForeachCanBeConvertedToQueryUsingAnotherGetEnumerator
-                foreach (var ingredient in _selectedRecipe.ingredients)
-                {
-                    if (!input.Contains(ingredient.item, ingredient.count))
-                    {
-                        // Our input inventory doesn't have everything we need to keep working. We're done.
-                        return;
-                    }
-                }
-
-                // Remove all the ingredients from from the inventory for the selected recipe.
-                foreach (var ingredient in _selectedRecipe.ingredients)
-                {
-                    // Remove the items from the inventory.
-                    input.Remove(ingredient.item, ingredient.count);
-                }
-
-                // Our input inventory has everything we need to keep working on this recipe.
-                _currentRecipe = _selectedRecipe;
-                _energyNeededToCompleteCurrentRecipe = _currentRecipe.energy;
-            }
-
-            // Work on the current recipe.
-            _energyNeededToCompleteCurrentRecipe -= 20;
-
-            // Check if the current recipe was completed.
-            if (_energyNeededToCompleteCurrentRecipe == 0)
-            {
-                // Store all the resulting items in the output inventory.
-                foreach (var result in _currentRecipe.results)
-                {
-                    output.Store(result.item, result.count);
-                }
-
-                // Mark the current recipe as completed and reset it for the next tick.
-                _currentRecipe = null;
+                WorkActiveRecipe();
             }
         }
     }
